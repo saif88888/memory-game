30e3d1494e2271cf07b40b3c64a1e8461b5d0631
saif88888 <saif_saeed_cs@yahoo.com>
2020-06-24 02:08:01 +0100

    Updates

---
+++ precache-manifest.0b2eda7226960596b7c71b51a5bdbd0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb97ff165b9a77958b47c8351fc1a4c4",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "970090023ac7cb2a0e6e",
+    "url": "/memory-game/static/css/main.812bb331.chunk.css"
+  },
+  {
+    "revision": "d77c9d463cee3de121c7",
+    "url": "/memory-game/static/js/2.dac25e42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.dac25e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970090023ac7cb2a0e6e",
+    "url": "/memory-game/static/js/main.7de6519a.chunk.js"
+  },
+  {
+    "revision": "d8e1235abbd88a9da6de",
+    "url": "/memory-game/static/js/runtime-main.5c0098dd.js"
+  }
+]);
